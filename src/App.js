d84c03837749595aa124476d3f67e59cd50de75d
King Josaphat Chewa <26507383+Kingjosh007@users.noreply.github.com>
2023-03-13 10:03:30 +0100

    feat: create ComingSoon page

--- src/App.js
+++ src/App.js
@@ -60,8 +60,6 @@ function App() {
         </div>   
         <ScrollToTop />
             <Route exact path='/' component={ Home } /> 
-            <Route path='/home2' component={ Home2 } /> 
-            <Route path='/home3' component={ Home3 } />
             <Route path='/header_style_02' component={ Home2_Header2 } />
             <Route path='/header_style_03' component={ Home3_Header3 } />
             <Route path="/presentation" component={Presentation} />
---
+++ src/components/pages/ComingSoon.js
@@ -0,0 +1,31 @@
+import React, { Component } from 'react';
+import Header from '../layout/Header';
+
+export class ComingSoon extends Component {
+    render() {
+        return (
+            <div className="site-main">
+                <Header />
+                <div className="ttm-page-title-row">
+                    <div className="container-xl">
+                        <div className="row">
+                            <div className="col-md-12">
+                                <div className="title-box text-center">
+                                    <div className="page-title-heading">
+                                        <h1 className="title">En maintenance</h1>
+                                    </div>
+                                    <div className="breadcrumb-wrapper">
+                                        <span>La page que vous souhaitez consulter subit quelques modifications et sera bientôt disponible.</span>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                {/* page-title end*/}
+            </div>
+        )
+    }
+}
+
+export default ComingSoon;
